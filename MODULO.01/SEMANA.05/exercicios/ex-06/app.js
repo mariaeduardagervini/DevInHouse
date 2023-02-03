873b2e5d3a6dc479a79962cc204143481ba48473
Maria Eduarda <mariaeduardagervini@hotmail.com>
2023-02-03 20:30:31 -0300

    up

---
+++ MODULO.01/SEMANA.05/exercicios/ex-06/app.js
@@ -0,0 +1,10 @@
+import Juros from './Juros.js';
+
+const aplicacao1 = new Juros(10000, 0.07, 24);
+const aplicacao2 = new Juros(10000, 0.15, 10);
+
+console.log(aplicacao1.calcularJurosSimples());
+console.log(aplicacao1.calcularJurosCompostos());
+
+console.log(aplicacao2.calcularJurosSimples());
+console.log(aplicacao2.calcularJurosCompostos());
