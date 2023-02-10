674c3cf539e235464f33faf3844685245b9dc0c2
Maria Eduarda <mariaeduardagervini@hotmail.com>
2023-02-10 20:03:20 -0300

    update

---
+++ MODULO.01/SEMANA.06/exercicios/ex-05/app.js
@@ -0,0 +1,16 @@
+function obterClienteNoBancoDeDados(idCliente) {
+  return new Promise((resolve, reject) => {
+    const cliente = { nome: 'bruce wayne', id: idCliente };
+    resolve(cliente);
+  });
+}
+
+async function processar() {
+  try {
+    const cliente = await obterClienteNoBancoDeDados(7);
+    console.log(cliente);
+  } catch (error) {
+    console.log(error);
+  }
+}
+processar();
