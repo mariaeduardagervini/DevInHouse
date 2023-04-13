daea91ab3c77a5d6c739241d46401f8339164b8d
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-13 16:59:40 -0300

    [M2S02] Ex 1 - Soma de números

---
+++ MODULO.02/SEMANA.02/exercicios/ex.01/ex01/Program.cs
@@ -0,0 +1,17 @@
+﻿	internal class Program
+{
+	private static void Main(string[] args)
+	{
+	int numero1, numero2, soma;
+		Console.WriteLine("Digite o primeiro número: ");
+		numero1 = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("Digite o segundo número: ");
+		numero2 = int.Parse(Console.ReadLine());
+
+		soma = numero1 + numero2;
+
+		Console.WriteLine($"A soma dos números é: {soma}");
+
+	}
+}
