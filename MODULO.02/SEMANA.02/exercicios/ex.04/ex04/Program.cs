8c3de005fce4c95935aa0e41f52a0fdb36bb755f
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-13 17:47:20 -0300

    [M2S02] Ex 4 - Informações de veículos

---
+++ MODULO.02/SEMANA.02/exercicios/ex.04/ex04/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		string marca, modelo;
+		double kmsRodados;
+
+		Console.WriteLine("Digite a marca do veículo: ");
+		marca = Console.ReadLine();
+
+		Console.WriteLine("Digite o modelo do veículo: ");
+		modelo = Console.ReadLine();
+		
+		Console.WriteLine("Digite a quantidade de quilometros rodados do veículo: ");
+		kmsRodados = double.Parse(Console.ReadLine());
+
+		Console.WriteLine($"\n - Marca do veículo: {marca}\n - Modelo do veículo: {modelo}\n - Quilometragem rodada: {kmsRodados}");
+
+		if(kmsRodados > 10000) {
+			Console.WriteLine("É necessário fazer a REVISÃO do veículo!");
+		}
+
+	}
+}
