20a22a5564f48a6929d72d91e11f0bf63013dcec
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-13 17:34:38 -0300

    [M2S02] Ex 3 - Nome e Idade

---
+++ MODULO.02/SEMANA.02/exercicios/ex.03/ex03/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		string nome;
+		int idade;
+
+		Console.WriteLine("Digite o nome do paciente: ");
+		nome = Console.ReadLine();
+
+		Console.WriteLine("Digite a idade do paciente: ");
+		idade = int.Parse(Console.ReadLine());
+
+		if(idade < 18) 
+		{
+			Console.WriteLine($"O paciente {nome} é MENOR de idade, pois tem {idade} anos");
+		} else if (idade >= 60) {
+			Console.WriteLine($"O paciente {nome} é IDOSO, pois tem {idade} anos");
+		} else 
+		{
+			Console.WriteLine($"O paciente {nome} é MAIOR de idade, pois tem {idade} anos");
+		}
+	}
+}
