d1ffc252bf882a5b3ee5603743ed0bbb1e94749b
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-13 17:18:33 -0300

    [M2S02] Ex 2 - Par ou ímpar

---
+++ MODULO.02/SEMANA.02/exercicios/ex.02/ex02/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+	int numero;
+
+	Console.WriteLine("Digite um número inteiro: ");
+	numero = int.Parse(Console.ReadLine());
+
+	if (numero % 2 == 0) 
+	{
+		Console.WriteLine("O número é par!");
+	} else 
+	{
+		Console.WriteLine("O número é ímpar!");
+	}
+	}
+}
