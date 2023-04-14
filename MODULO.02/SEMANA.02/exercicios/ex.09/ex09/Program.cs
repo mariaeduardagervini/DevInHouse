4d4992dd4bead3d583b1624d4a5f50276812f44c
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-13 21:56:02 -0300

    [M2S02] Ex 9 - Média aritmética

---
+++ MODULO.02/SEMANA.02/exercicios/ex.09/ex09/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int quantidade, soma;
+		double media;
+
+		Console.WriteLine("Digite a quantidade de números que você deseja para calcular a média aritmética: ");
+		quantidade = int.Parse(Console.ReadLine());
+
+		int[] numeros = new int(quantidade);
+
+		for (int i = 0; i < numeros.Length; i++) {
+			Console.WriteLine("Digite o número desejado: ");
+			numeros[i] = int.Parse(Console.ReadLine());
+		}
+		foreach (var item in numero)
+		{
+			soma += item;
+		}
+		soma = double.Parse(Console.WriteLine());
+		Console.WriteLine($"A média aritmética dos números digitados é: {media}");
+	}	
+}
