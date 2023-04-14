dda06de9fa24f9849bbfb8ea0f39f06dfc84979d
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-13 21:04:42 -0300

    [M2S02] Ex 5 - Ordenando números

---
+++ MODULO.02/SEMANA.02/exercicios/ex.05/ex05/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int[] numeros = new int[4];
+
+		for(int i = 0; i < numeros.Length; i++) {
+		Console.Write("Digite um número inteiro: ");
+		numeros[i] = int.Parse(Console.ReadLine());
+		}
+		Array.Sort(numeros);
+
+	foreach (var item in numeros)
+	{
+		Console.Write($"{item} ");
+	}
+	}
+}
