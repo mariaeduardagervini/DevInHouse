bf7543c7a2adacf57b1298c41e91426f2232f570
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-13 21:26:50 -0300

    [M2S02] Ex 7 - Números pares

---
+++ MODULO.02/SEMANA.02/exercicios/ex.06/ex06/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		string frase = "A linguagem de programação C# é muito poderosa";
+		List<string> palavra = new List<string> (frase.Split(""));
+
+		foreach (var item in palavra)
+		{
+			if(item == "poderosa") {
+				Console.WriteLine(item);
+			}
+		}
+		
+	}
+}
---
+++ MODULO.02/SEMANA.02/exercicios/ex.07/ex07/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int numero;
+		do
+		{
+			Console.WriteLine("Digite um número inteiro: ");
+			numero = int.Parse(Console.ReadLine());
+
+			if(numero <= 0) {
+			Console.WriteLine("Erro! O número deve ser positivo e diferente de zero");
+			}
+		} while (numero <= 0);
+
+		Console.WriteLine($"Números pares até {numero}");
+
+		for(int i = 0; i <= numero; i+= 2 ) {
+		Console.Write(i + " ");
+		} 
+	}	
+	}
