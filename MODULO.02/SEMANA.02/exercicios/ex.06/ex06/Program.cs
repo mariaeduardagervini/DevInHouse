fd4220ae56369543603db87632650ca6c1222b07
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-15 08:24:50 -0300

    [M2S02] Ex 6 - Separando palavras

--- MODULO.02/SEMANA.02/exercicios/ex.06/ex06/Program.cs
+++ MODULO.02/SEMANA.02/exercicios/ex.06/ex06/Program.cs
@@ -3,14 +3,8 @@
 	private static void Main(string[] args)
 	{
 		string frase = "A linguagem de programação C# é muito poderosa";
-		List<string> palavra = new List<string> (frase.Split(""));
+		string[] palavras = frase.Split(' ');
 
-		foreach (string item in palavra)
-		{
-			if(item == "poderosa") {
-				Console.Write(item);
-			}
-		}
-		
+		Console.WriteLine(palavras[7]);		
 	}
 }
