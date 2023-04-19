333b0d30dad2d788039f560f105facf18e942fca
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-19 16:12:00 -0300

    [M2S03] Ex 6 - Crie Instâncias para a classe carro

--- MODULO.02/SEMANA.03/exercicios/ex03ate10/Program.cs
+++ MODULO.02/SEMANA.03/exercicios/ex03ate10/Program.cs
@@ -1,16 +1,37 @@
 ﻿using Classes;
 
 var proprietario1 = new Proprietario();
-proprietario.Nome = "Maria";
-proprietario.CPF = "001.002.003-04";
-proprietario.Telefone = "3333-0000";
+proprietario1.Nome = "Maria";
+proprietario1.CPF = "001.002.003-04";
+proprietario1.Telefone = "3333-0000";
 
 var proprietario2 = new Proprietario();
-proprietario.Nome = "João";
-proprietario.CPF = "004.003.002-01";
-proprietario.Telefone = "2222-0000";
+proprietario2.Nome = "João";
+proprietario2.CPF = "004.003.002-01";
+proprietario2.Telefone = "2222-0000";
 
 var proprietario3 = new Proprietario();
-proprietario.Nome = "Sandra";
-proprietario.CPF = "000.003.008-07";
-proprietario.Telefone = "3444-0000";
+proprietario3.Nome = "Sandra";
+proprietario3.CPF = "000.003.008-07";
+proprietario3.Telefone = "3444-0000";
+
+var carro1 = new Carro();
+carro1.Marca = "Volkswagen";
+carro1.Modelo = "Nivus";
+carro1.Placa = "MMM5E24";
+carro1.Cor = "Branca";
+carro1.Proprietario = proprietario1;
+
+var carro2 = new Carro();
+carro2.Marca = "Fiat";
+carro2.Modelo = "Toro";
+carro2.Placa = "JHG8A75";
+carro2.Cor = "Preta";
+carro2.Proprietario = proprietario2;
+
+var carro3 = new Carro();
+carro3.Marca = "Ford";
+carro3.Modelo = "Focus";
+carro3.Placa = "GHI9E35";
+carro3.Cor = "Prata";
+carro3.Proprietario = proprietario3;
