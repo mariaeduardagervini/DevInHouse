8ec78f83efff6ff2c0121fbddb90ed48055f9c6b
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-19 16:02:58 -0300

    [M2S03] Ex 4 - Crie Instâncias para a classe Propr

--- MODULO.02/SEMANA.03/exercicios/ex03ate10/Program.cs
+++ MODULO.02/SEMANA.03/exercicios/ex03ate10/Program.cs
@@ -1,3 +1,16 @@
 ﻿using Classes;
 
+var proprietario1 = new Proprietario();
+proprietario.Nome = "Maria";
+proprietario.CPF = "001.002.003-04";
+proprietario.Telefone = "3333-0000";
 
+var proprietario2 = new Proprietario();
+proprietario.Nome = "João";
+proprietario.CPF = "004.003.002-01";
+proprietario.Telefone = "2222-0000";
+
+var proprietario3 = new Proprietario();
+proprietario.Nome = "Sandra";
+proprietario.CPF = "000.003.008-07";
+proprietario.Telefone = "3444-0000";
