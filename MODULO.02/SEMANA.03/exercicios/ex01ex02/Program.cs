9deeacbde6364da718f1af09096ebef199020bfa
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-19 15:20:59 -0300

    [M2S03] Ex 1 - Criar Método Soma de números

---
+++ MODULO.02/SEMANA.03/exercicios/ex01ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		int SomaNumeros(int num1, int num2)
+		{
+ 		return num1 + num2;
+		}
+		int resultado = SomaNumeros(5,8);
+		Console.WriteLine($" O resultado é: {resultado}");
+	}
+}
