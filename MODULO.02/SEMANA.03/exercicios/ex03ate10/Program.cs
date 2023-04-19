4d3f674c409a84f54baad95794fb45925c16e845
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-19 15:57:52 -0300

    [M2S03] Ex 3 - Crei a classe Proprietario

--- MODULO.02/SEMANA.03/exercicios/ex03ate10/Program.cs
+++ MODULO.02/SEMANA.03/exercicios/ex03ate10/Program.cs
@@ -1,6 +1,3 @@
-﻿public class Proprietario
-    {
-        public string Nome {get; set;}
-        public string CPF {get; set;}
-        public string Telefone {get; set;}
-	}
+﻿using Classes;
+
+
---
+++ MODULO.02/SEMANA.03/exercicios/ex03ate10/Proprietario.cs
@@ -0,0 +1,10 @@
+namespace Classes
+{
+	public class Proprietario
+    {
+        public string Nome {get; set;}
+        public string CPF {get; set;}
+        public string Telefone {get; set;}
+	}
+}
+
