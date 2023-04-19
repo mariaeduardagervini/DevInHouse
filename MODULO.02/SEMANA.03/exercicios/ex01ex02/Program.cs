572a5e720e45dbfde68acba3e7eb1e59d98542db
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-19 15:48:47 -0300

    [M2S03] Ex 2 - Criar Sobrecargas do método Soma

--- MODULO.02/SEMANA.03/exercicios/ex01ex02/Program.cs
+++ MODULO.02/SEMANA.03/exercicios/ex01ex02/Program.cs
@@ -1,12 +1,27 @@
 ﻿internal class Program
 {
-	private static void Main(string[] args)
-	{
-		int SomaNumeros(int num1, int num2)
+	static int SomaNumeros(int num1, int num2)
 		{
  		return num1 + num2;
 		}
-		int resultado = SomaNumeros(5,8);
-		Console.WriteLine($" O resultado é: {resultado}");
+	static double SomaNumeros (double num1, double num2)
+		{
+    	return num1 + num2;
+		}
+ 	static double SomaNumeros (double num1, double num2, double num3)
+		{
+    	return num1 + num2 + num3;
+		}
+	private static void Main(string[] args)
+	{
+		
+		int resultado1 = SomaNumeros(5,8);
+		Console.WriteLine(resultado1);
+
+		 double resultado2 = SomaNumeros(5.2,8.1);
+		 Console.WriteLine(resultado2);
+
+		 double resultado3 = SomaNumeros(5.2,7.2,1.1);
+		 Console.WriteLine(resultado3);
 	}
 }
