3a47940b27fdb75891d080cc2d5705ce07b12b37
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-19 16:05:20 -0300

    [M2S03] Ex 5 - Crie a classe Carro

---
+++ MODULO.02/SEMANA.03/exercicios/ex03ate10/Carro.cs
@@ -0,0 +1,11 @@
+namespace Classes
+{
+	public class Carro
+    {
+        public string Marca{get; set;}
+        public string Modelo {get; set;}
+        public string Placa {get; set;}
+        public string Cor {get; set;}
+        public string Proprietario {get; set;}
+	}
+}
