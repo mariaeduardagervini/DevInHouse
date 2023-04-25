4f23f3d66d92e3f099cad267fd3ed6a94ba0136a
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-04-24 22:31:56 -0300

    aula 01

---
+++ MODULO.02/SEMANA.04/aula.01/Exemplo.cs
@@ -0,0 +1,17 @@
+public class Exemplo
+{
+
+public string nome;
+public int cpf; 
+}
+
+// Principais modificadores de acesso
+
+// PUBLIC: todos os membros(atributos = nome e cpf, métodos, classes), pode ser acessado por qualquer parte do código.
+
+// PROTECTED: os membros podem ser acessados através da própria classe e pelas classes derivadas.
+
+// PRIVATE: todos os membros só podem ser acessados pela própria classe.
+
+// INTERNAL: é por padrão. Se colocar antes da classe ou não, já é por padrão internal.
+
---
+++ MODULO.02/SEMANA.04/aula.01/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program {
+
+	private static void Main(string[] args) 
+	{
+		Console.WriteLine("Hello, World!");
+
+		Exemplo exemplo01 = new Exemplo(); // Instanciando um objeto da classe Exemplo
+
+		exemplo01.nome = "Maria";
+		exemplo01.cpf = 123;
+
+	}
+}
+
+// Encapsulamento
+
+// Proteger dados de uma classe
+// GET = Ler
+// SET = Alterar
+
+// Permissionar, impedir que sejam acessados diretamente
