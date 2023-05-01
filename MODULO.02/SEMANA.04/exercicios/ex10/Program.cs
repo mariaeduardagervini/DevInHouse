bc2102dd7b9a57e65160b29974fed8999c727493
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-01 18:46:37 -0300

    [M2S04] Ex 10 - Criação de uma classe de Conta Ba

--- MODULO.02/SEMANA.04/exercicios/ex10/Program.cs
+++ MODULO.02/SEMANA.04/exercicios/ex10/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex10
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria();
+
+            conta.Deposito(500);
+            Console.WriteLine($"Saldo: {conta.VerificarSaldo()}");
+
+            conta.Retirada(200);
+            Console.WriteLine($"Saldo: {conta.VerificarSaldo()}");
+
+            conta.Deposito(300);
+            Console.WriteLine($"Saldo: {conta.VerificarSaldo()}");
+
+            System.Console.WriteLine("Transações Realizadas:");
+
+            foreach (string transacao in conta.VerificarTransacoes())
+            {
+                System.Console.WriteLine(transacao);
+            }
+        }
+    }
+}
