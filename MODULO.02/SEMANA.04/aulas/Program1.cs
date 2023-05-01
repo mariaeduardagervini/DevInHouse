434963f6b0dd42e18c7a2f83938dce3c38b9ef39
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-01 18:00:08 -0300

    [M2S04] Ex 4 - Encapsulamento ContaBancaria

--- MODULO.02/SEMANA.04/aulas/Program1.cs
+++ MODULO.02/SEMANA.04/aulas/Program1.cs
@@ -1,4 +1,3 @@
-using Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- MODULO.02/SEMANA.04/exercicios/ex04/Program.cs
+++ MODULO.02/SEMANA.04/exercicios/ex04/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex04
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			ContaBancaria conta = new ContaBancaria();
+
+			conta.Depositar(1000);
+
+			Console.WriteLine("Saldo após o depósito: " + conta.Saldo);
+
+			// Saque maior que o saldo disponivel
+			bool saque1 = conta.Sacar(2000);
+			Console.WriteLine("Saque de R$ 2000,00" + (saque1 ? " realizado com sucesso!" : " não foi possível realizar o saque"));
+
+			//Saque menor que o saldo disponivel
+			bool saque2 = conta.Sacar(500);
+			Console.WriteLine("Saque de R$ 500,00" + (saque2 ? " realizado com sucesso!" : " não foi possível realizar o saque"));
+
+			Console.WriteLine("Saldo após os saques: " + conta.Saldo);
+
+		}
+	}
+}
