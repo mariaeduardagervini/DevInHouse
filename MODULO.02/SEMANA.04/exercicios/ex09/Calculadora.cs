da694267b400b5c9633323d3069434ed4ef57764
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-01 18:43:09 -0300

    [M2S04] Ex 9 - Calculadora: classe estática

---
+++ MODULO.02/SEMANA.04/exercicios/ex09/Calculadora.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex09
+{
+    public static class Calculadora
+    {
+        public const double Pi = 3.14159;
+
+        public static int Soma(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtracao(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static int Divisao(int a, int b)
+        {
+            
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Divisão por zero não é permitida");
+            }
+            
+            return a / b;
+        }
+
+    }
+}
--- MODULO.02/SEMANA.04/exercicios/ex09/Program.cs
+++ MODULO.02/SEMANA.04/exercicios/ex09/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex09
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine("Valor de Pi: " + Calculadora.Pi);
+
+            int a = 8;
+            int b = 6;
+
+            System.Console.WriteLine("Soma: " + Calculadora.Soma(a, b));
+            System.Console.WriteLine("Subtração: " + Calculadora.Subtracao(a, b));
+            System.Console.WriteLine("Multiplicação: " + Calculadora.Multiplicacao(a, b));
+            System.Console.WriteLine("Divisão: " + Calculadora.Divisao(a, b));
+        }
+    }
+}
