8b20add1f28b7e564c774df4cac193d6951222fe
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-01 18:30:58 -0300

    [M2S04] Ex 7 - Constante Valor Máximo Conta

--- MODULO.02/SEMANA.04/exercicios/ex07/Program.cs
+++ MODULO.02/SEMANA.04/exercicios/ex07/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex07
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			ContaBancaria conta = new ContaBancaria();
+
+			conta.NumeroConta = 321;
+			conta.Depositar(1000);
+			conta.Sacar(500);
+			conta.Depositar(6000);
+			conta.Sacar(4000);
+		}
+	}
+}
