efe6a96d3aff4db4dda362a81b762abb621a9c5f
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-01 18:08:15 -0300

    [M2S04] Ex 5 - Encapsulamento Classe Pessoa

--- MODULO.02/SEMANA.04/exercicios/ex05/Program.cs
+++ MODULO.02/SEMANA.04/exercicios/ex05/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ex05
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Pessoa pessoa1 = new Pessoa();
+			pessoa1.Nome = "Maria";
+			pessoa1.DefinirIdade(29);
+
+			Console.WriteLine($"Nome: {pessoa1.Nome}" );
+			Console.WriteLine($"Idade: {pessoa1.Idade} anos" );
+		}
+	}
+}
