53766746b4acfdb308a3c8cd1ec83b3bc1780335
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-18 16:04:35 -0300

    [M2S06] Ex 2 - Preparando os testes

---
+++ MODULO.02/SEMANA.06/exercicios/ex01/exercicio01.teste/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace exercicio01.teste;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
