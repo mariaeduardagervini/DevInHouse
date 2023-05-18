8732bb98980bd2fe2ae8bcc861ca2f7e34384ca0
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-18 16:52:14 -0300

    [M2S06] Ex 6 - Praticando lógica de programação P2

---
+++ MODULO.02/SEMANA.06/exercicios/ex01/ex06/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Digite os números: ");
+
+		List<int> lista = new List<int>() {2,6,7,9,1,-4,-3,888};
+		int menor;
+		int maior;
+
+		for(int i = 0; i < lista.Count; i++)
+		{
+			Console.WriteLine("\n--------------------\n");
+			foreach (var item in lista)
+			{
+				Console.WriteLine(item);
+			}
+			for (int j = i; j < lista.Count; j ++)
+			{
+				while(lista[i] > lista[j]){
+					menor = lista[j];
+					maior = lista[i];
+					lista[i] = menor;
+					lista[j] = maior;
+				}
+			}
+		}
+		foreach (var item in lista)
+			{
+				Console.WriteLine(item);
+			}
+		
+	}
+}
