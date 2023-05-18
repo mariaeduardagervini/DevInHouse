46a706f51676741987d3666c06cc9c898558628d
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-05-18 16:37:40 -0300

    [M2S06] Ex 5 - Praticando lógica de programação P1

--- MODULO.02/SEMANA.06/exercicios/ex01/exercicio01/Program.cs
+++ MODULO.02/SEMANA.06/exercicios/ex01/exercicio01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		double[] valores = new double[] {2, -5, 2.1, 3};
+		List<double> listaOrdenada = new List<double>();
+
+		for (int i = valores.Count() -1; i >= 0; i --)
+		{
+			listaOrdenada.Add(valores[i]);
+		}
+		foreach (var item in listaOrdenada)
+		{
+			Console.WriteLine(item);
+		}
+
+	}
+}
