0e5ddeee13c6f7f5209e34eb83cfa5707c43f427
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-07-21 15:30:01 -0300

    updates

---
+++ MODULO.03/SEMANA.01/Escola.API/DataBase/DbContexto.cs
@@ -0,0 +1,25 @@
+﻿using Escola.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Escola.API.DataBase
+{
+    public class DbContexto : DbContext
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Db-auth");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().HasKey(x => x.Login);
+            modelBuilder.Entity<Usuario>().Property(x => x.Login)
+                                           .HasMaxLength(50);
+
+            modelBuilder.Entity<Usuario>().Property(x => x.Senha)
+                                          .HasMaxLength(50);
+        }
+    }
+}
---
+++ MODULO.03/SEMANA.01/Escola.API/Model/TokenCliente.cs
@@ -0,0 +1,9 @@
+﻿namespace Escola.API.Model
+{
+    public class TokenCliente
+    {
+        public string Token { get; set; }
+        public string NomeCliente { get; set; }
+        public int IdCliente { get; set; }
+    }
+}
