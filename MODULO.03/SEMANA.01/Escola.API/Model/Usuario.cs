9880c54aa8b120da0546d8ef24eb851c76e22f12
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-07-21 14:56:52 -0300

    [M3S02] Ex1 - Criar Modelo de usuários

---
+++ MODULO.03/SEMANA.01/Escola.API/Model/Usuario.cs
@@ -0,0 +1,34 @@
+﻿using Escola.API.DTO;
+
+namespace Escola.API.Model
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Permissao { get; set; }
+        public string Senha { get; set; }
+        public bool Interno { get; set; }
+        public Usuario()
+        {
+
+        }
+        public Usuario(UsuarioGetDTO usuario)
+        {
+            Nome = usuario.Nome;
+            Login = usuario.Login;
+            Permissao = usuario.Permissao;
+            Interno = usuario.Interno;
+        }
+        public Usuario(UsuarioDTO usuario) : this((UsuarioGetDTO)usuario)
+        {
+            Senha = usuario.Senha;
+        }
+
+        public void Update(Usuario usuario)
+        {
+            Nome = usuario.Nome;
+            Permissao = usuario.Permissao;
+        }
+    }
+}
