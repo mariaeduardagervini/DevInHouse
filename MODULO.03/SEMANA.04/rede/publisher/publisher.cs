a4a43793aa6f7a5ae760595add24aaea1f523edb
Maria Eduarda Gervini <mariaeduardagervini@hotmail.com>
2023-08-08 17:05:23 -0300

    update aula 03 semana 04

--- MODULO.03/SEMANA.04/rede/publisher/publisher.cs
+++ MODULO.03/SEMANA.04/rede/publisher/publisher.cs
@@ -2,22 +2,23 @@ using System;
 using System.Net.Sockets;
 using System.Text;
 
+
 class Program
 {
-	static void Main()
-	{
-		string Ip = "subscriber";
-		int Port =  4546;
+    static void Main()
+    {
+        string Ip = "subscriber-audaces"; 
+        int Port = 4546;
 
-		//steam
-		using (var client = new TcpClient(Ip,Port));
-		using(var stream = client.GetStream());
-
-		string message = "Essa mensagem foi emitida pelo publisher!";
-		byte[] data = Encoding.UTF8.GetBytes(message);
-
-		steam.Write(data, 0, data.Length);
-		Console.WriteLine("Mensagem Enviada com Sucesso!");
-
-	}
+        //steam
+        using (var client = new TcpClient(Ip,Port))
+        using (var stream = client.GetStream())
+       {
+        string message = "Essa mensagem foi emitida pelo publisher!";
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Mensagem Enviada com Sucesso!!!");
+        } 
+        
+    }
 }
